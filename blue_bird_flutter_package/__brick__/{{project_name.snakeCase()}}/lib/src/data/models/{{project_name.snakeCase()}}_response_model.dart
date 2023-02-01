3f8b01b70a597e6329c7b0b7348d77c67829f384
Abel1027 <arodriguezm1027@gmail.com>
2023-02-01 12:29:56 +0100

    [flutter_package] Convert fromJson into a factory constructor

--- blue_bird_flutter_package/__brick__/{{project_name.snakeCase()}}/lib/src/data/models/{{project_name.snakeCase()}}_response_model.dart
+++ blue_bird_flutter_package/__brick__/{{project_name.snakeCase()}}/lib/src/data/models/{{project_name.snakeCase()}}_response_model.dart
@@ -7,15 +7,15 @@ class {{project_name.pascalCase()}}ResponseModel {
     required this.accessToken,
   });
 
-  /// [accessToken] doc
-  final String accessToken;
-
-  /// [fromJson] doc
-  {{project_name.pascalCase()}}ResponseModel fromJson(
+  /// [{{project_name.pascalCase()}}ResponseModel.fromJson] doc
+  factory {{project_name.pascalCase()}}ResponseModel.fromJson(
     Map<String, dynamic> json,
   ) {
     return {{project_name.pascalCase()}}ResponseModel(
       accessToken: json['access-token'] as String,
     );
   }
+
+  /// [accessToken] doc
+  final String accessToken;
 }
--- blue_bird_flutter_package/__brick__/{{project_name.snakeCase()}}/lib/{{project_name.snakeCase()}}.dart
+++ blue_bird_flutter_package/__brick__/{{project_name.snakeCase()}}/lib/{{project_name.snakeCase()}}.dart
@@ -1,5 +1,6 @@
 library {{project_name.snakeCase()}};
 
+export 'src/di/{{project_name.snakeCase()}}_provider.dart';
 export 'src/domain/entities/{{project_name.snakeCase()}}_entity.dart';
 export 'src/presentation/pages/{{project_name.snakeCase()}}_page.dart';
 export 'src/presentation/widgets/{{project_name.snakeCase()}}_widget.dart';
